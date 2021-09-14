f62f4d06dbe52b16a98c3535d21df381eac63e10
Hasan Tareq <hsntareq@gmail.com>
2021-09-14 23:49:49 +0600

    item edit & delete

--- includes/Admin/Protocol_List.php
+++ includes/Admin/Protocol_List.php
@@ -35,10 +35,64 @@ class Protocol_List extends \WP_List_Table {
 				break;
 
 			default:
-				return isset( $item->column_name ) ? $item->column_name : '';
+				return isset( $item->$column_name ) ? $item->$column_name : '';
 		}
 	}
 
+	public function column_name( $item ) {
+		$actions           = array();
+		$actions['edit']   = sprintf(
+			'<a href="%s" title="%s">%s</a>',
+			add_query_arg(
+				array(
+					'page'   => 'biodrop-portal',
+					'action' => 'edit',
+					'id'     => $item->id,
+				),
+				admin_url( 'admin.php' )
+			),
+			__( 'Edit', 'sponsor-portal' ),
+			__( 'Edit', 'sponsor-portal' ),
+		);
+		$actions['delete'] = sprintf(
+			'<a class="submitdelete" onclick="return confirm(\'Are you confirm?\');" href="%s" title="%s">%s</a>',
+			wp_nonce_url(
+				add_query_arg(
+					array(
+						'page'   => 'biodrop-portal',
+						'action' => 'sp-po-delete-action',
+						'id'     => $item->id,
+					),
+					admin_url( 'admin-post.php' )
+				)
+			),
+			__( 'Edit', 'sponsor-portal' ),
+			__( 'Edit', 'sponsor-portal' ),
+		);
+
+		return sprintf(
+			'<a href="%1$s"><strong>%2$s</strong></a> %3$s',
+			add_query_arg(
+				array(
+					'page'   => 'biodrop-portal',
+					'action' => 'view',
+					'id'     => $item->id,
+				),
+				admin_url( 'admin.php' )
+			),
+			$item->name,
+			$this->row_actions( $actions )
+		);
+
+	}
+
+	protected function column_cb( $item ) {
+		return sprintf(
+			'<input type="checkbox" name="protocol_id[]" value="$d">',
+			$item->id
+		);
+
+	}
 
 
 	public function prepare_items() {
